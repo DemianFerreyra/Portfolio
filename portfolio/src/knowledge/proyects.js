70070a3e7894133534303944e81193800738cff9
DemianFerreyra <demiancra@gmail.com>
2022-05-13 21:29:30 -0300

    Actualizacion

--- portfolio/src/knowledge/proyects.js
+++ portfolio/src/knowledge/proyects.js
@@ -1,3 +1,13 @@
+const PI = {
+    name: "Individual Proyect: Foods",
+    desc: "Individual proyect maded for 'SoyHenry' bootcamp. A SPA using React/Redux for the front, and postgre/sequalize for the back.Used an API called 'Spoonacular' and also maded one myself. Application not deployed",
+    link: "https://github.com/DemianFerreyra/INSTANT",
+    images: [
+        "PI1.png",
+        "PI2.png",
+        "PI3.png"
+    ]
+}
 const instant = {
     name: "Practice proyect: Instant",
     desc: "Instagram like application (but not an exact clon), using React/Redux and a local Json database. No external API's. Application not deployed",
@@ -10,5 +20,5 @@ const instant = {
 }
 
 export const proyectos = [
-    instant
+    PI, instant
 ]
